b5f42c5db201f554bbe67795c9cecd0e91f67bb6
quanyuan ruan <muronglengjing@163.com>
2020-02-02 11:58:40 +0800

    改变了地图绘制的形式，增加了一个矩形用于碰撞检测

--- script/map.py
+++ script/map.py
@@ -2,38 +2,37 @@ import pygame
 from pygame.locals import *
 
 
-class MapObject(pygame.sprite.Sprite):
-    def __init__(self, key, x, y):
+class MapDraw(pygame.sprite.Sprite):
+    def __init__(self, maps):
         super().__init__()
-        dic = {0: "glass"}
-        self.image = pygame.image.load("resource\{}.png".format(dic[key]))
-        self.image = pygame.transform.scale2x(self.image)
-        self.rect = Rect((x*72, y*72), (72, 72))
+        self.dict = {1: "glass", 2: "null"}
+        self.image = pygame.Surface([1080, 720])
+        self.image.set_colorkey([0, 0, 0])
+        self.rect = pygame.Rect((0, 0), (0, 0))
+        c, r = 0, 0
+        for i in maps:
+            for j in i:
+                if j == 0:
+                    pass
+                else:
+                    image = pygame.image.load(r"resource\{}.png".format(self.dict[j]))
+                    image = pygame.transform.scale2x(image)
+                    rect = pygame.Rect((72 * r, 72 * c), (72, 72))
+                    self.rect.union(rect)
+                    print(72 * r, 72 * c)
+                    self.image.blit(image, rect)
+                r = r + 1
+            r = 0
+            c = c + 1
 
 
 class Map(pygame.sprite.Group):
-    def __init__(self, maps):
+    def __init__(self, backward_map, forward_map=[[0 for i in range(15)] for i in range(10)]):
         super().__init__()
-        c, r = 0, 0
-        for maps_raw in maps:
-            for maps_object in maps_raw:
-                mo = MapObject(maps[r][c], c, r)
-                self.add(mo)
-                c = c + 1
-            c = 0
-            r = r + 1
-
-    def map_move(self, x_to_map, y_to_map):
-        if x_to_map > self.X_map - 540:
-            lift = -(self.X_map - 540)
-        elif x_to_map < 540:
-            lift = 0
-        else:
-            lift = x_to_map - 540
-        if y_to_map > self.Y_map - 540:
-            up = -(self.X_map - 540)
-        elif y_to_map < 360:
-            up = 0
-        else:
-            up = y_to_map - 360
-        return lift, up
+        # 背景
+        m_b = MapDraw(backward_map)
+        # 前景
+        m_f = MapDraw(forward_map)
+        self.add(m_b, m_f)
+        # 前景允许精灵碰撞
+        self.forward_map_rect = m_f.rect
